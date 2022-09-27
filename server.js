68ccadf2d3a4eb1e4b6302872eea5938c48af28e
Boobeh123 <boobeh321@gmail.com>
2022-09-27 12:02:56 -1000

    Troubleshoot heroku deployment

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/', async (request, response) => {
-    // const todoItems = await db.collection('stretches').find().toArray()
-    // const itemsLeft = await db.collection('stretches').countDocuments()
+    const todoItems = await db.collection('stretches').find().toArray()
+    const itemsLeft = await db.collection('stretches').countDocuments()
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
     // db.collection('todos').find().toArray()
     // .then(data => {
